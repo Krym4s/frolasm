91ea73b82f9d4b2c44e13fce5e762821ab320a4a
Krym4s <70252345+Krym4s@users.noreply.github.com>
2020-12-20 03:31:31 +0300

    Add files via upload

---
+++ DED_ASM/cmake-build-debug/CMakeFiles/DED_ASM/cmake-build-debug/CMakeFiles/DED_ASM.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DED_ASM.dir/IsE_Assembler.cpp.obj"
+  "CMakeFiles/DED_ASM.dir/IsE_Stack.cpp.obj"
+  "CMakeFiles/DED_ASM.dir/IsE_text.cpp.obj"
+  "CMakeFiles/DED_ASM.dir/main.cpp.obj"
+  "DED_ASM.exe"
+  "DED_ASM.exe.manifest"
+  "DED_ASM.pdb"
+  "libDED_ASM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DED_ASM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
