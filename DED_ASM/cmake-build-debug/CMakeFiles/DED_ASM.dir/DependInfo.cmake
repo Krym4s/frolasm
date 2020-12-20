772b6dc0f7218a77cf6d5e35cbc738c420dc28e9
Krym4s <70252345+Krym4s@users.noreply.github.com>
2020-12-20 03:03:41 +0300

    Add files via upload

---
+++ DED_ASM/cmake-build-debug/CMakeFiles/DED_ASM.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/egolg/CLionProjects/DED_ASM/cmake-build-debug/IsE_Assembler.cpp" "C:/Users/egolg/CLionProjects/DED_ASM/cmake-build-debug/CMakeFiles/DED_ASM.dir/IsE_Assembler.cpp.obj"
+  "C:/Users/egolg/CLionProjects/DED_ASM/cmake-build-debug/IsE_Stack.cpp" "C:/Users/egolg/CLionProjects/DED_ASM/cmake-build-debug/CMakeFiles/DED_ASM.dir/IsE_Stack.cpp.obj"
+  "C:/Users/egolg/CLionProjects/DED_ASM/cmake-build-debug/IsE_text.cpp" "C:/Users/egolg/CLionProjects/DED_ASM/cmake-build-debug/CMakeFiles/DED_ASM.dir/IsE_text.cpp.obj"
+  "C:/Users/egolg/CLionProjects/DED_ASM/main.cpp" "C:/Users/egolg/CLionProjects/DED_ASM/cmake-build-debug/CMakeFiles/DED_ASM.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
